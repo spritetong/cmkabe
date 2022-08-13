3828b4e1d787a10768638e0e28a139e775b534cf
Sprite Tong <spritetong@gmail.com>
2022-08-13 21:50:55 +0800

    Bump 0.2.1

--- env.mk
+++ env.mk
@@ -12,7 +12,7 @@ ifndef __ENV_MK__
 __ENV_MK__ = $(abspath $(lastword $(MAKEFILE_LIST)))
 CMKABE_HOME := $(abspath $(dir $(__ENV_MK__)))
 
-CMAKEABE_VERSION = 0.2.0
+CMAKEABE_VERSION = 0.2.1
 
 # ==============================================================================
 # = Environment Variables
