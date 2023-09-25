1c4b6dd044b29939424d3875258eb9db21123f79
Sprite Tong <spritetong@gmail.com>
2023-09-26 07:02:52 +0800

    Split rmakelib.py from shlutilib.py

--- rmake.py
+++ rmake.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     import os
     import sys
     sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-    from shlutilib import RsyncMake
+    from rmakelib import RsyncMake
     sys.exit(RsyncMake.main(os.path.basename(__file__)))
